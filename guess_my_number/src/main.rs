839b50e361e82ea2261bcd098c946fd24425a221
Tangrenin <quentindidier@msn.com>
2021-11-03 22:36:17 +0100

    feat: user safe input, random generation, comparing value with random number

--- guess_my_number/src/main.rs
+++ guess_my_number/src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    // Génération de nombres aléatoires
+    let mut rng = thread_rng();
+    let nombre_secret: u32 = rng.gen_range(1..101);
+    println!("Le nombre à deviner : {}", nombre_secret);
+
+
+    println!("Devine mon nombre ! (entre 1 et 100) \n\nSaisissez votre proposition : ");
+
+    loop {
+        // Saisie de l'utilisateur
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+
+        // On vérifie que l'utilisateur a rentré un nombre valide
+        let input = match input.trim().parse::<u32>() {
+            Ok(val) =>  val,
+            Err(_) => {eprintln!("Erreur : entrez un nombre entier positif :"); continue}
+        };
+
+        // Comparaison avec le nombre secret
+        match input.cmp(&nombre_secret) {
+            Ordering::Less => println!("Votre proposition est trop petite ! Réessayez :"),
+            Ordering::Equal => {println!("Bravo ! Vous avez trouvé !"); break},
+            Ordering::Greater => println!("Votre proposition est trop grande ! Réessayez :"),
+        }
+    }
+
+
 }
