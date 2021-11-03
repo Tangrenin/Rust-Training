25dd241a09ea25d2b49eca68e769395e91125bc5
Tangrenin <quentindidier@msn.com>
2021-11-03 22:44:56 +0100

    feat: added coloring prints

--- guess_my_number/src/main.rs
+++ guess_my_number/src/main.rs
@@ -1,15 +1,14 @@
 use std::io;
 use rand::{thread_rng, Rng};
 use std::cmp::Ordering;
+use colored::*;
 
 fn main() {
     // Génération de nombres aléatoires
     let mut rng = thread_rng();
     let nombre_secret: u32 = rng.gen_range(1..101);
-    println!("Le nombre à deviner : {}", nombre_secret);
 
-
-    println!("Devine mon nombre ! (entre 1 et 100) \n\nSaisissez votre proposition : ");
+    println!("{}","\n\nDevine mon nombre ! (entre 1 et 100) \n\nSaisissez votre proposition : ".purple());
 
     loop {
         // Saisie de l'utilisateur
@@ -19,14 +18,14 @@ fn main() {
         // On vérifie que l'utilisateur a rentré un nombre valide
         let input = match input.trim().parse::<u32>() {
             Ok(val) =>  val,
-            Err(_) => {eprintln!("Erreur : entrez un nombre entier positif :"); continue}
+            Err(_) => {eprintln!("{}", "Erreur : entrez un nombre entier positif !".red()); continue}
         };
 
         // Comparaison avec le nombre secret
         match input.cmp(&nombre_secret) {
-            Ordering::Less => println!("Votre proposition est trop petite ! Réessayez :"),
-            Ordering::Equal => {println!("Bravo ! Vous avez trouvé !"); break},
-            Ordering::Greater => println!("Votre proposition est trop grande ! Réessayez :"),
+            Ordering::Less => println!("{}", "Votre proposition est trop petite ! Réessayez :".red()),
+            Ordering::Equal => {println!("{}", "Bravo ! Vous avez trouvé !".green()); break},
+            Ordering::Greater => println!("{}", "Votre proposition est trop grande ! Réessayez :".red()),
         }
     }
 
